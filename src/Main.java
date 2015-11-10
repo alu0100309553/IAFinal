cff1e688cb7c3d4f46bc5850f79bf77525f254c8
Rubén Labrador <alu0100309553@ull.edu.es>
2015-11-10 18:28:37 +0000

    casi acabado

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.awt.EventQueue;
-import java.awt.MouseInfo;
-public class Main {
 
+public class Main {
 	public static void main(String[] args) {
 		ventana ventana1 = new ventana();
 		ventana1.frame.setVisible(true);
